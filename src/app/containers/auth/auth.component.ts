45aa5e905a6e30a187840e5f8eece34c48d55148
Travis Ueki <travis@mag.cr>
2017-01-10 10:18:16 -0800

    hubspot auth

--- src/app/containers/auth/auth.component.ts
+++ src/app/containers/auth/auth.component.ts
@@ -22,12 +22,13 @@ export class Auth {
   localAuth: string = 'auth/local';
   tokenAuth: string = 'auth/token';
   endpoint: string = 'auth/local';
+  hubAuthAPI: string = 'https://app.hubspot.com/oauth/authorize?client_id=4341ae1b-abed-4c31-99b5-712a2c1f4b38&scope=contacts%20automation&redirect_uri=https://magn3tic.github.io/Portal-client/#/';
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  authenticate() {
-    window.localStorage.getItem(this.JWT_KEY) === 'null' || 'undefined' ? this.endpoint = this.localAuth : this.endpoint = this.tokenAuth;
-    this.authService.authenticate(this.endpoint, this.user)
-    .subscribe(()=>this.router.navigate(['']))
-  }
+  // authenticate() {
+  //   window.localStorage.getItem(this.JWT_KEY) === 'null' || 'undefined' ? this.endpoint = this.localAuth : this.endpoint = this.tokenAuth;
+  //   this.authService.authenticate(this.endpoint, this.user)
+  //   .subscribe(()=>this.router.navigate(['']))
+  // }
 }; 

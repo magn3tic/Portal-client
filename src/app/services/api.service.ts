b0064a024427221d6ad244bbddaeeaf2556cb043
Travis Ueki <travis@mag.cr>
2017-01-19 17:13:12 -0800

    updates

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -42,6 +42,7 @@ export class ApiService {
             .map(this.checkForError)
             .catch(err => Observable.throw(err))
             .map(this.getJson)
+            .map(json => console.log('ApiService.get json: ', json))
     }
 
     post(path: string, body?: any): Observable<any> {
